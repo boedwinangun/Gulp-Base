522dddb05b7f39a4524bdc2d09f3b93ac6fe8c48
solvedia <boedwinangun@gmail.com>
2017-08-23 18:50:06 +0700

    Update gulpfile.js, add task error reporting

--- gulpfile.js
+++ gulpfile.js
@@ -1,52 +1,93 @@
-// Konfigurasi
-var gulp        = require('gulp');
-var sass        = require('gulp-sass');
-var browserSync = require('browser-sync').create();
-var reload      = browserSync.reload;
-var plumber     = require('gulp-plumber');
-var notify      = require('gulp-notify');
-var clean       = require('gulp-clean');
-var cssnano     = require('gulp-cssnano');
-var concat      = require('gulp-concat');
-var uglify      = require('gulp-uglify');
-var imagemin    = require('gulp-imagemin');
-var htmlmin     = require('gulp-htmlmin');
-var zip         = require('gulp-zip');
-var gutil       = require('gulp-util');
-var marge       = require('marge-stream');
+/**
+ *
+ * Konfigurasi Gulp.js
+ * @author [Boed Winangun]
+ * @version [1.0]
+ * 
+ */
+var gulp        = require('gulp'),
+	browserSync = require('browser-sync').create(),
+	reload      = browserSync.reload,
+	notify      = require('gulp-notify'),
+	plumber     = require('gulp-plumber'),
+	gutil       = require('gulp-util'),
+	path        = require('path'),
+	scss        = require('gulp-sass'),
+	clean       = require('gulp-clean'),
+	cssnano     = require('gulp-cssnano'),
+	concat      = require('gulp-concat'),
+	uglify      = require('gulp-uglify'),
+	imagemin    = require('gulp-imagemin'),
+	htmlmin     = require('gulp-htmlmin'),
+	merge       = require('merge-stream'), 
+	zip         = require('gulp-zip');
 
-// Compile SCSS
-gulp.task('sass', function(){
-	return gulp.src('./app/scss/*.scss')
-    	//.pipe(sass().on('error', sass.logError))
-		.pipe(plumber({
-			errorHandler:function(err){
-				notify.onError({
-					title : "Gulp error in " + err.plugin,
-					message : err.toString()
-				})(err)
-			}
-		}))
-		.pipe(sass())
-		.pipe(plumber.stop())
-		.pipe(gulp.dest('./app/css'))
-		.pipe(reload({stream: true}));
+/**
+ *
+ * error reporter object
+ *
+ */
+var reportError = function (error) { 
+    var lineNumber = (error.lineNumber) ? 'LINE ' + error.lineNumber + ' -- ' : '';
+    var pluginName = (error.plugin) ? ': ['+error.plugin+']' : '['+currentTask+']';
+ 
+    notify({
+        title: 'Task Failed '+ pluginName,
+		icon: path.join(__dirname, 'gulp.png'),
+        message: lineNumber + 'Lihat console.'
+    }).write(error);
+ 
+    gutil.beep();
+ 
+    var report = '';
+    var chalk = gutil.colors.white.bgRed;
+ 
+    report += chalk('TASK:') + pluginName+'\n';
+    report += chalk('ERROR:') + ' ' + error.message + '\n';
+    if (error.lineNumber) { report += chalk('LINE:') + ' ' + error.lineNumber + '\n'; }
+    if (error.fileName) { report += chalk('FILE:') + ' ' + error.fileName + '\n'; }
+ 
+    console.error(report);
+ 
+    this.emit('end');
+}
+
+/**
+ *
+ * Compile SCSS
+ *
+ */
+gulp.task('scss', function(){
+	return gulp.src('./app/scss/**.scss')
+	.pipe(plumber({
+	    errorHandler: reportError
+	}))
+	.pipe(scss())
+	.pipe(gulp.dest('./app/css'))
+	.pipe(notify({ message: 'SCSS task complete' }))
+	.pipe(reload({stream: true}));
 });
 
-// Task Clean Build Directory
+/**
+ *
+ * Task Clean Build Directory
+ *
+ */
 gulp.task('cleanBuild', function(){
 	return gulp.src('build', {read: false})
 	.pipe(clean());
 });
 
-// Deploy ke folder Build
+/**
+ *
+ * Deploy ke folder Build
+ *
+ */
 gulp.task('deploy',['cleanBuild'], function(){
 	// optimasi css
 	var cssOptimize = gulp.src('app/css/*.css')
-		// .pipe(gutil.log('css Optimize Start'))
 		.pipe(cssnano())
-		// .pipe(gutil.log('css Optimize Finish'))
-		.pipe(gulp.dest('build/css/'));
+		.pipe(gulp.dest('build/css'));
 
 	// Menggabungkan semua file js dan optimasi
 	var jsOptimize = gulp.src('app/js/*.js')
@@ -71,14 +112,22 @@ gulp.task('deploy',['cleanBuild'], function(){
 	return merge(cssOptimize,jsOptimize,imgOptimize,htmlOptimize,fonts);
 })
 
-// Deploy ke file Zip
+/**
+ *
+ * Deploy ke file Zip
+ *
+ */
 gulp.task('deployZip',['deploy'], function(){
 	var zipNow = gulp.src('build/**')
 		.pipe(zip('deploy.zip'))
 		.pipe(gulp.dest('build'));
 })
 
-// Task Local Webserver dan sinkronisasi dengan browser
+/**
+ *
+ * Task Local Webserver dan sinkronisasi dengan browser
+ *
+ */
 gulp.task('default', function(){
 	browserSync.init({
 		server: {
@@ -86,5 +135,5 @@ gulp.task('default', function(){
 		}
 	});
 	gulp.watch('./app/**/*').on('change', reload);
-	gulp.watch('./app/scss/*.scss',['sass']);
+	gulp.watch('./app/scss/**/*.scss',['scss']);
 });
